afb74dcde2d920b3a1f46c0aae6afaaec8352488
AjAxRiCoO8 <Ricardo-42-ajax@live.nl>
2016-07-19 13:29:25 +0200

    More Enemy changes

--- InsaneKillerArcher/InsaneKillerArcher/InsaneKillerArcher/Enemy.cs
+++ InsaneKillerArcher/InsaneKillerArcher/InsaneKillerArcher/Enemy.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,9 +8,29 @@ namespace InsaneKillerArcher
 {
     class Enemy : AnimatedGameObject
     {
+
+        private Vector2 startPosition = new Vector2(InsaneKillerArcher.Screen.X + 20, InsaneKillerArcher.Screen.Y - 40);
+        private float movementSpeed = 200;
+
         public Enemy() : base(0, "Enemy")
         {
-            //this.LoadAnimation("")
+            this.LoadAnimation("spr_enemy_strip2@2x1", "walkingLeft", true);
+            this.LoadAnimation("spr_enemy_dead_strip5@5x1", "dead", false);
+            this.LoadAnimation("spr_enemy_fight_strip2@2x1", "fighting", true);
+
+            this.PlayAnimation("walkingLeft");
+
+            base.position = startPosition;
+            velocity = new Vector2(-movementSpeed, 0);
+
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+
+
+
+            base.Update(gameTime);
         }
     }
 }
