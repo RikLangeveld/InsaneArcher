351e60869b4551442599f498b384e3dc7c394a6a
RikLangeveld <rik_langeveld@hotmail.com>
2016-07-19 15:38:06 +0200

    Add Arrow Class

---
+++ InsaneKillerArcher/InsaneKillerArcher/InsaneKillerArcher/Arrow.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InsaneKillerArcher
+{
+    class Arrow : SpriteGameObject
+    {
+
+        public Arrow(string assetname, Vector2 position) : base(assetname)
+        {
+            this.position = position;
+            velocity = new Vector2(100f, 0f);
+        }
+    }
+}
