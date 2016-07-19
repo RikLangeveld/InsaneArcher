40cb3da404532184195eedbd469184389996bb72
RikLangeveld <rik_langeveld@hotmail.com>
2016-07-19 13:56:02 +0200

    Add Angle to PlayerWeapon,

--- InsaneKillerArcher/InsaneKillerArcher/InsaneKillerArcher/PlayerWeapon.cs
+++ InsaneKillerArcher/InsaneKillerArcher/InsaneKillerArcher/PlayerWeapon.cs
@@ -2,14 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace InsaneKillerArcher
 {
     class PlayerWeapon : SpriteGameObject
     {
+        private float angle;
+
         public PlayerWeapon(string assetname) : base(assetname)
         {
+        }
 
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(sprite.Sprite, GlobalPosition, null, Color.White, angle, origin, 1.0f, SpriteEffects.None, 0);
         }
     }
 }
